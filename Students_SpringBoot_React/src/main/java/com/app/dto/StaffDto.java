eebc464fac1f1d49a0e747e67f74a9fcb590c274
Yogesh <Yogesh@YOGESH-DADA>
2026-03-25 12:54:59 +0530

    Entity and Dto added for Staff

---
+++ Students_SpringBoot_React/src/main/java/com/app/dto/StaffDto.java
@@ -0,0 +1,14 @@
+package com.app.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class StaffDto {
+
+	private Long id;
+	private String staffName;
+	private String staffAddress;
+	private String staffDesignation;
+}
---
+++ Students_SpringBoot_React/src/main/java/com/app/entity/Staff.java
@@ -0,0 +1,23 @@
+package com.app.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Staff {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String staffName;
+	private String staffAddress;
+	private String staffDesignation;
+
+}
